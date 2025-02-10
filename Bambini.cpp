b524d0d2b914ca551749f7d1136cc79da6f27ada
Nias26 <genangeliludovico26@gmail.com>
2025-02-10 09:05:42 +0100

    Proseguimento Esercizio
    Sviluppatore alla tastiera: Ludovico Genangeli

--- Bambini.cpp
+++ Bambini.cpp
@@ -1,67 +1,71 @@
 #include "funzioni.h"
-#define SIZE 50
 
 /*
-Questa applicazione deve poter permettere l'inserimento dei nuovi bambini, la stampa di tutti i bambini,
-la ricerca di un bambino,
-il salvataggio su file dei dati, lettura dei file da dati, ordinamento dei bambini in base ad un criterio.
+Questa applicazione deve poter permettere l'inserimento dei nuovi bambini, la
+stampa di tutti i bambini, la ricerca di un bambino, il salvataggio su file dei
+dati, lettura dei file da dati, ordinamento dei bambini in base ad un criterio.
 */
 
+int main() {
+  Bambino bambini[SIZE];
+  int nBambini = 0;
+  char choice;
+  try {
+    do {
+      cout << "Benvenuto alla scuola Marchesi" << endl
+           << "1. Iscrivi nuovo bambino" << endl
+           << "2. Stampa tutti i bambini" << endl
+           << "3. Filtra bambini" << endl
+           << "4. Esporta lista bambini" << endl
+           << "5. Importa lista bambini" << endl
+           << "6. Oridna bambini" << endl;
+      cout << "-> ";
+      cin >> choice;
+      switch (choice) {
+      case '1': {
+        string nome, cognome, citta;
+        Sezioni sezione;
+        int eta;
 
-int main(){
-    Bambino bambini[SIZE];
-    int nBambini = 0;
-    char choice;
-    try{
-    do{
-        cout << "Benvenuto alla scuola Marchesi" << endl
-             << "1. Iscrivi nuovo bambino" << endl
-             << "2. Stampa tutti i bambini" << endl
-             << "3. Filtra bambini" << endl
-             << "4. Esporta lista bambini" << endl
-             << "5. Importa lista bambini" << endl
-             << "6. Oridna bambini" << endl;
-        cout << "-> ";
-        cin >> choice;
-        switch(choice){
-            case '1':{
-                string nome, cognome, citta;
-                Sezioni sezione;
-                int eta;
-  
-                cout << "Inserire i dati del nuovo bambino: " << endl
-                     << "Nome: ";
-                cin >> nome;
-                cout << "Cognome: ";
-                cin >> cognome;
-                cout << "Citta: ";
-                cin >> citta;
-                cout << "Sezione(A,B,C,D,E,F): ";
-                cin >> sezione;
-                cout << "Eta: ";
-                cin >> eta;
-  
-                bambini[nBambini](nome, cognome, citta, sezione, eta);
-                ++nBambini;
-                break;
-            }
-            case '2':{
-                stampaBambini(nBambini, bambini);
-                break;
-            }
-            case 'q':{
-                cout << "Arrivederci!" << endl;
-                break;
-            }
-            default:{
-                cout << "Scelta non valida" << endl;
-            }
-        }
+        cout << "Inserire i dati del nuovo bambino: " << endl << "Nome: ";
+        cin >> nome;
+        cout << "Cognome: ";
+        cin >> cognome;
+        cout << "Citta: ";
+        cin >> citta;
+        cout << "Sezione(A,B,C,D,E,F): ";
+        cin >> sezione;
+        cout << "Eta: ";
+        cin >> eta;
 
-    }while(choice != 'q');
-    }catch(BambinoNonValido err){
-        cerr << err.msg << endl;
-    }
+        bambini[nBambini](nome, cognome, citta, sezione, eta);
+        ++nBambini;
+        break;
+      }
+      case '2': {
+        stampaBambini(nBambini, bambini);
+        break;
+      }
+      case '3': {
+        string query;
+        cout << "Filtra per nome: ";
+        cin >> query;
+        stampaBambino(cercaBambino(query, bambini), bambini);
+        break;
+      }
+      case 'q': {
+        cout << "Arrivederci!" << endl;
+        break;
+      }
+      default: {
+        cout << "Scelta non valida" << endl;
+      }
+      }
 
-    return 0;
+    } while (choice != 'q');
+  } catch (BambinoNonValido err) {
+    cerr << err.msg << endl;
+  }
+
+  return 0;
 }
--- funzioni.h
+++ funzioni.h
@@ -2,18 +2,47 @@
 #define H_FUNZIONI
 
 #include "Bambini.h"
+#include <cctype>
+#define SIZE 50
 
-void stampaBambini(const int CURRENT_SIZE, Bambino b[]){
-    for(int i = 0; i < CURRENT_SIZE; ++i){
-        cout << "[ " << endl;
-        cout << "\tNome: " << b[i].getNome() << endl
-            << "\tCognome: " << b[i].getCognome() << endl
-            << "\tCitta: " << b[i].getCitta() << endl
-            << "\tSezione: " << b[i].getSezione() << endl
-            << "\tEta: " << b[i].getEta() << endl;
-        cout << "[ " << endl;
-    }
+void stampaBambini(const int CURRENT_SIZE, const Bambino b[]) {
+  for (int i = 0; i < CURRENT_SIZE; ++i) {
+    cout << "[ " << endl;
+    cout << "\tNome: " << b[i].getNome() << endl
+         << "\tCognome: " << b[i].getCognome() << endl
+         << "\tCitta: " << b[i].getCitta() << endl
+         << "\tSezione: " << b[i].getSezione() << endl
+         << "\tEta: " << b[i].getEta() << endl;
+    cout << "[ " << endl;
+  }
 }
 
+void stampaBambino(const int INDEX, const Bambino b[]) {
+  cout << "[ " << endl;
+  cout << "\tNome: " << b[INDEX].getNome() << endl
+       << "\tCognome: " << b[INDEX].getCognome() << endl
+       << "\tCitta: " << b[INDEX].getCitta() << endl
+       << "\tSezione: " << b[INDEX].getSezione() << endl
+       << "\tEta: " << b[INDEX].getEta() << endl;
+  cout << "[ " << endl;
+}
+
+string toupperString(string str) {
+  for (int i = 0; i < str.length(); ++i) {
+    toupper(str[i]);
+  }
+  return str;
+}
+
+int cercaBambino(string name_filter, const Bambino b[]) {
+  for (int i = 0; i < SIZE; ++i) {
+    if (toupperString(b[i].getNome()) == toupperString(name_filter)) {
+      return i;
+    } else {
+      cerr << "Bambino non trovato" << endl;
+      return 0;
+    }
+  }
+}
 
 #endif
